cfdb9db16ddc1507b7785219ba55a6033550cd86
Anna Kazantseva <annetastara@gmail.com>
2019-11-04 11:56:59 +0200

    Updates

---
+++ precache-manifest.fe72e721a7b993b3f337afa15cff09f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efb3f9fba4f5da43ea9e79554f73fc1d",
+    "url": "/react_dynamic-list-of-todos/index.html"
+  },
+  {
+    "revision": "bd048e9842429281eab0",
+    "url": "/react_dynamic-list-of-todos/static/css/main.4ce09124.chunk.css"
+  },
+  {
+    "revision": "a1ce7d43c27ebdf27559",
+    "url": "/react_dynamic-list-of-todos/static/js/2.627e1a72.chunk.js"
+  },
+  {
+    "revision": "bd048e9842429281eab0",
+    "url": "/react_dynamic-list-of-todos/static/js/main.0af02958.chunk.js"
+  },
+  {
+    "revision": "04977e40ef1cd22bcb98",
+    "url": "/react_dynamic-list-of-todos/static/js/runtime~main.b6f1ef06.js"
+  }
+]);
